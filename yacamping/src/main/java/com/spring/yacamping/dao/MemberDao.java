59a4ffe1e9f3aee9cc6b037421a97718cc9d7562
gihyonsd <gihyonsd@gmail.com>
2022-08-04 19:45:58 +0900

    마이페이지, db 데이터 넣기, 카카오맵 해결

---
+++ yacamping/src/main/java/com/spring/yacamping/dao/MemberDao.java
@@ -0,0 +1,15 @@
+package com.spring.yacamping.dao;
+
+ 
+
+import org.springframework.stereotype.Repository;
+
+import com.spring.yacamping.domain.MemberVO;
+
+@Repository
+public interface MemberDao {
+
+    public int Login(MemberVO vo) throws Exception;
+    public void register(MemberVO vo) throws Exception;
+    public MemberVO readMember(String id) throws Exception;
+}
---
+++ yacamping/src/main/java/com/spring/yacamping/serviceImpl/MemberServiceImpl.java
@@ -0,0 +1,45 @@
+package com.spring.yacamping.serviceImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.yacamping.dao.MemberDao;
+import com.spring.yacamping.domain.MemberVO;
+import com.spring.yacamping.service.MemberService;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+
+    @Autowired
+    private MemberDao memberDao;
+
+    @Override
+    public int Login(MemberVO vo) throws Exception {
+    
+        return memberDao.Login(vo);
+
+    }
+
+	@Override
+	public void register(MemberVO vo) throws Exception {
+		memberDao.register(vo);
+	}
+
+	@Override
+	public MemberVO readMember(String id) {
+		System.out.println("S : readMember()실행");
+		MemberVO vo = null;
+		
+		try {
+			vo = memberDao.readMember(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return vo;
+	}
+
+
+}
