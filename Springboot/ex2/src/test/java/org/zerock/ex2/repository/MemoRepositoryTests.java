1f47ff0cf2a81078ff4c5a73d9b1473bb0ae23d5
gihyonsd <gihyonsd@gmail.com>
2022-09-24 19:15:29 +0900

    JpaRepository

---
+++ Springboot/ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -0,0 +1,47 @@
+package org.zerock.ex2.repository;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.ex2.entity.Memo;
+
+
+import javax.swing.border.MatteBorder;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemoRepositoryTests {
+
+    @Autowired
+    MemoRepository memoRepository;
+
+    @Test
+    public void testInsert() {
+        System.out.println(memoRepository.getClass().getName());
+    }
+
+    @Test
+    public void testInsertDummies(){
+        IntStream.rangeClosed(1,100).forEach(i -> {
+            Memo memo = Memo.builder().memoText("memo"+i).build();
+            memoRepository.save(memo);
+        });
+    }
+
+    @Test
+    public void testUpdate() {
+        Memo memo = Memo.builder().mno(1L).memoText("update............1").build();
+        System.out.println("1====================");
+        memoRepository.save(memo);
+        System.out.println("2====================");
+    }
+
+    @Test
+    public void testDelete() {
+
+        Long mno = 100L;
+
+        memoRepository.deleteById(mno);
+    }
+}
